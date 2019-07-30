7fd70afb06531c8bf3e7de990e70ef7f3c6f16d7
whj <emailatwen@gmail.com>
2019-07-30 21:00:11 +0800

    update

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,8 +1,62 @@
+import { getType } from '../common/common'
+
+interface Configs {
+  baseUrl?: string;
+  url: string;
+  method: string;
+  headers?: { [key: string]: string };
+  data?: any;
+  timeout?: number;
+  responseType?: string;
+}
+
+new XMLHttpRequest().send()
+
+type Data = string | Document | Blob | ArrayBufferView | ArrayBuffer | FormData | URLSearchParams | ReadableStream<Uint8Array> | null
 
 class Req {
-  static create (): Req {
-    return new Req
+  constructor() {
+    
   }
+
+  baseUrl = '';
+
+  timeout = 18000;
+
+  before (fn: Function) {
+
+  }
+
+  after (callback: Function) {
+
+  }
+
+  request(configs: Configs) {
+
+  }
+
+  get (url: string, data?: Data) {
+    return this.request({
+      url, method: 'get', data
+    })
+  }
+
+  post () {
+
+  }
+
+  put () {
+
+  }
+
+  patch () {
+
+  }
+
+  delete () {
+
+  }
+
 }
 
 
